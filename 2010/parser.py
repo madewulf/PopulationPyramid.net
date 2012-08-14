ed6f38698987548bd6c617d421ec917a7abc7070
Martin De Wulf <madewulf@gmail.com>
2012-08-14 09:55:22 +0200

    parser.py generating all json data now

--- 2010/parser.py
+++ 2010/parser.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
 import csv
+import urllib
+import json
 from collections import defaultdict
 files = {
     "male":
@@ -13,59 +16,84 @@ files = {
             }
 }
 
-ages = set([])
-years = set([])
-country_set = set([])
-country_key_set = set([])
-
-#dict['burundi'][2010][male][0]
 f = lambda: defaultdict(f)
 res_dict = defaultdict(f)
+year_set = set()
 
+countries_dict = {}
 count =0
 for sex in files:
     for typ in files[sex]:
         reader = csv.reader(open(files[sex][typ], 'rb'), delimiter=';')
         for row in reader:
-            year = row[5]
-            country = row[2]
-            i=0
-            for v in row[6:]:
-                res_dict[country][year][sex][i]=v
-                i = i+1
-                count = count +1
+            year = int(row[5])
+            if (year % 5== 0):
+                year_set.add(year)
+                country = row[2]
+                encoded_country =  urllib.quote_plus(country.decode('ascii','ignore'))
+                countries_dict[encoded_country]=country
+                i=0
+                for v in row[6:]:
+                    try:
+                        res_dict[encoded_country][year][sex][i]=int(v)
+                    except:
+                        if v!="…":
+                            print "failed on :'%s' " % v
+                        res_dict[encoded_country][year][sex][i]=0
+                    i = i+1
+                    count = count +1
 
 
-print res_dict['EUROPE']['2095']['male'][0]
-print "woohhooo -----------------------------------", count
+year_list = sorted(list(year_set))
+
+#print year_list
+#print countries_dict
+
+encoded_countries_list= sorted(countries_dict.keys())
+alphabet = map(chr, range(97, 123))
+
+letters_to_countries_list_dict = defaultdict(list)
+
+for c in encoded_countries_list:
+    first_letter = c[0]
+    letters_to_countries_list_dict[first_letter].append(c)
+
+#print letters_to_countries_list_dict
+
+pop_dict = defaultdict(f)
+for encoded_country in encoded_countries_list:
+    for year in year_list:
+        for sex in ('male','female'):
+            sum =0
+            for i in res_dict[encoded_country][year][sex]:
+                sum += res_dict[encoded_country][year][sex][i]
+            pop_dict[encoded_country][year]=sum
+
+#print pop_dict
 def age_to_int(age):
     if not age.strip().startswith("100"):
         return int(age.split('-')[0])
     else:
         return 100
 
-age_list.sort(key=age_to_int)
+for encoded_country in encoded_countries_list:
+    f = open("generated/%s.js" % encoded_country,'w')
+    f.write(json.dumps(res_dict[encoded_country]))
+    f.close()
 
-year_list = list(years)
-year_list.sort()
+age_labels = [' 0-4', ' 5-9', ' 10-14', ' 15-19', ' 20-24', ' 25-29', ' 30-34', ' 35-39', ' 40-44', ' 45-49', ' 50-54', ' 55-59', ' 60-64', ' 65-69', ' 70-74', ' 75-79', ' 80-84', ' 85-89', ' 90-94', ' 95-99', ' 100+'];
 
-data = {}
-for year in dict:
-    total_pop = sum([dict[year]['Total'][age] for age in age_list])
 
-    data[year] = {}
+main_data_dict ={
+    'alphabet':alphabet,
+    'lettersToCountriesList':letters_to_countries_list_dict,
+    'populations':pop_dict,
+    'years':year_list,
+    'countriesHumanNames': countries_dict,
+    'ageLabels':age_labels
 
-    data[year]['M'] = [dict[year]['Male'][age] / total_pop for age in age_list]
-    data[year]['F'] = [dict[year]['Female'][age] / total_pop for age in age_list]
+}
 
-data_file = open("data.js", "w")
-data_file.write("var data = ")
-data_file.write(str(data))
-data_file.write(";\n")
-data_file.write("labels = ")
-data_file.write(str(age_list))
-data_file.write(";\n")
-data_file.write("years = ")
-data_file.write(str(year_list))
-data_file.write(";")
-data_file.close()
+f = open('mainData.json','w')
+f.write(json.dumps(main_data_dict))
+f.close()
